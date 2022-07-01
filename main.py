63ea1cd310168a2ff5c530e31b73dba1f54abb08
dotLight <snoftlt@yahoo.com>
2022-07-01 15:01:28 +0430

    Bug solved!

--- main.py
+++ main.py
@@ -6,34 +6,37 @@ def floatArray(arr):
         for j in range(len(arr[0])):
             arr[i][j] = float(arr[i][j])
 def powerSigma(arr):
-    sigma = 0
-    for i in range(len(arr)):
-        for j in range(len(arr[0])):
-            sigma += (arr[i][j]**2)
-    sigma = math.sqrt(sigma)
+    sigma = []
+    for i in range(len(arr[0])):
+        sigma.append(0)
+        for j in range(len(arr)):
+            sigma[i] += (arr[j][i]**2)
+        sigma[i] = math.sqrt(sigma[i])
     return sigma
 def normalize(arr):
     sigma = powerSigma(arr)
     for i in range(len(arr)):
         for j in range(len(arr[0])):
-            arr[i][j] = (arr[i][j]/sigma)
+            arr[i][j] = (arr[i][j]/sigma[j])
 def distance(normalizedArr, normalUnnormalIdeal):
     distance = []
     for i in range(len(normalizedArr)):
         sigma = 0
         for j in range(len(normalizedArr[0])):
-            sigma += ((normalizedArr[i][j]-normalUnnormalIdeal)**2)
+            sigma += ((normalizedArr[i][j]-normalUnnormalIdeal[j])**2)
         sigma = math.sqrt(sigma)
         distance.append(sigma)
     return distance
 def normalizeIdeal(Ideal, unnormalizedArr):
-    return (Ideal/powerSigma(unnormalizedArr))
+    for i in range(len(Ideal)):
+        Ideal[i] = (Ideal[i]/powerSigma(unnoramlizedArr)[i])
+    return Ideal
 #LINMAP Function
-def LINMAP(CSVArr, IdealPoint):
+def LINMAP(CSVArr, IdealPoints):
     normalizedArr = copy.deepcopy(CSVArr)
-    IdealPoint = normalizeIdeal(IdealPoint, normalizedArr)
+    IdealPoints = normalizeIdeal(IdealPoints, normalizedArr)
     normalize(normalizedArr)
-    dplus = distance(normalizedArr, IdealPoint)
+    dplus = distance(normalizedArr, IdealPoints)
     print("LINMAP Point index is {i}".format(i = dplus.index(min(dplus))))
     print("LINMAP Point is {i}".format(i = CSVArr[dplus.index(min(dplus))]))
 
@@ -41,14 +44,14 @@ def LINMAP(CSVArr, IdealPoint):
 print("Enter Path of CSV file:")
 path = input()
 print("Enter Ideal Point:")
-IdealPoint = float(input())
+IdealPoints = float(input())
 print("Enter nonIdeal Point:")
-nonIdealPoint = float(input())
+nonIdealPoints = float(input())
 
 CSVFile = []
 with open(path, "r") as f:
     for i in f:
         CSVFile.append(i.replace(",", " ").split())
     floatArray(CSVFile)
-LINMAP(CSVFile, IdealPoint)
+LINMAP(CSVFile, IdealPoints)
 
